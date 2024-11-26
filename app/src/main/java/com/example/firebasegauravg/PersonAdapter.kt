235d80669456ce2a5e0aebfee1cf11f4d8760864
GauravChumber <gauravchumber20@gmail.com>
2024-11-26 00:03:51 -0500

    showing images in recycler view

--- app/src/main/java/com/example/firebasegauravg/PersonAdapter.kt
+++ app/src/main/java/com/example/firebasegauravg/PersonAdapter.kt
@@ -5,8 +5,10 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.storage.FirebaseStorage
 
 class PersonAdapter(options: FirebaseRecyclerOptions<Person>)
     : FirebaseRecyclerAdapter<Person, PersonAdapter.MyViewHolder>(options) {
@@ -21,6 +23,13 @@ class PersonAdapter(options: FirebaseRecyclerOptions<Person>)
         holder.txtName.text = model.name
         holder.txtRole.text = model.role
         holder.txtAge.text = model.age.toString()
+
+        val theImage: String = model.photo
+
+        val storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(theImage)
+        Glide.with(holder.imgPhoto.context)
+            .load(storageReference)
+            .into(holder.imgPhoto)
     }
 
     class MyViewHolder(inflater: LayoutInflater, parent: ViewGroup)
@@ -28,5 +37,6 @@ class PersonAdapter(options: FirebaseRecyclerOptions<Person>)
         val txtName: TextView = itemView.findViewById(R.id.txtName)
         val txtRole: TextView = itemView.findViewById(R.id.txtRole)
         val txtAge: TextView = itemView.findViewById(R.id.txtAge)
+        val imgPhoto: ImageView = itemView.findViewById(R.id.imgPhoto)
     }
 }
